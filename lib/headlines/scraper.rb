830df697a6e6491c80fdd319761deb47739db556
John Guest <jcguest90@gmail.com>
2019-10-17 04:43:30 -0500

    added attributes

--- lib/headlines/scraper.rb
+++ lib/headlines/scraper.rb
@@ -1,7 +1,8 @@
 class Headlines::Scraper
 
-    attr_accessor :name, :url, :headline1, :headline2, :headline3, :headline4, :headline5, 
-    :headline1_url, :headline2_url, :headline3_url, :headline4_url, :headline5_url, :headlines_ary, :url_ary
+    attr_accessor :name, :url, :headline1, :headline2, :headline3, :headline4, :headline5, :headline6, :headline7,
+    :headline1_url, :headline2_url, :headline3_url, :headline4_url, :headline5_url, :headline6_url, :headline7_url,
+    :headlines_ary, :url_ary
 
 
 
@@ -23,13 +24,19 @@ class Headlines::Scraper
         source.headline4_url = doc.search("#zone-left > div.zone__content > div:nth-child(3) > div > a").attribute("href").value
         source.headline5 = doc.search("#zone-left > div.zone__content > div:nth-child(4) > div > div > div > div > a > div").text.strip
         source.headline5_url = doc.search("#zone-left > div.zone__content > div:nth-child(4) > div > a").attribute("href").value
-       
+        source.headline6 = doc.search("#zone-left > div.zone__content > div:nth-child(5) > div > div > div > div > a > div").text.strip
+        source.headline6_url = doc.search("#zone-left > div.zone__content > div:nth-child(5) > div > a").attribute("href").value
+        source.headline7 = doc.search("#zone-left > div.zone__content > div:nth-child(6) > div > div > div > div > a > div").text.strip
+        source.headline7_url = doc.search("#zone-left > div.zone__content > div:nth-child(6) > div > a").attribute("href").value
+
         source.headlines_ary = []
         source.headlines_ary << source.headline1
         source.headlines_ary << source.headline2
         source.headlines_ary << source.headline3
         source.headlines_ary << source.headline4
         source.headlines_ary << source.headline5
+        source.headlines_ary << source.headline6
+        source.headlines_ary << source.headline7
 
         source.url_ary = []
         source.url_ary << source.headline1_url
@@ -37,6 +44,8 @@ class Headlines::Scraper
         source.url_ary << source.headline3_url
         source.url_ary << source.headline4_url
         source.url_ary << source.headline5_url
+        source.url_ary << source.headline6_url
+        source.url_ary << source.headline7_url
 
         Headlines::Source.new(source.name, source.url, source.headlines_ary, source.url_ary)
         source
@@ -52,14 +61,22 @@ class Headlines::Scraper
         source.headline1_url = doc.search("figcaption div h1 a").first["href"]
         source.headline2 = doc.search("figcaption div h1 a.black-ln")[2].text.strip
         source.headline2_url = doc.search("figcaption div h1 a.black-ln")[2]["href"]
+        source.headline3 = doc.search("figcaption div h1 a.black-ln")[3].text.strip
+        source.headline3_url = doc.search("figcaption div h1 a.black-ln")[3]["href"]
+        source.headline4 = doc.search("figcaption div h1 a.black-ln")[4].text.strip
+        source.headline3_url = doc.search("figcaption div h1 a.black-ln")[4]["href"]
         
         source.headlines_ary = []
         source.headlines_ary << source.headline1
         source.headlines_ary << source.headline2
+        source.headlines_ary << source.headline3
+        source.headlines_ary << source.headline4
 
         source.url_ary = []
         source.url_ary << source.headline1_url
         source.url_ary << source.headline2_url
+        source.url_ary << source.headline3_url
+        source.url_ary << source.headline4_url
 
         Headlines::Source.new(source.name, source.url, source.headlines_ary, source.url_ary)
         source
@@ -75,14 +92,26 @@ class Headlines::Scraper
         source.headline1_url = doc.search("body > a").attribute("href").value
         source.headline2 = doc.search("body > div.tb-c-story-previews.tb-c-story-previews--homepage > div > div.tb-o-grid > div:nth-child(1) > a > h2").text.strip
         source.headline2_url = doc.search("body > div.tb-c-story-previews.tb-c-story-previews--homepage > div > div.tb-o-grid > div:nth-child(1) > a").attribute("href").value
-        
+        source.headline3 = doc.search("body > div.tb-c-story-previews.tb-c-story-previews--homepage > div > div.tb-o-grid > div:nth-child(2) > a > h2").text.strip
+        source.headline3_url = doc.search("body > div.tb-c-story-previews.tb-c-story-previews--homepage > div > div.tb-o-grid > div:nth-child(2) > a").attribute("href").value
+        source.headline4 = doc.search("body > div.tb-c-story-previews.tb-c-story-previews--homepage > div > div.tb-o-grid > div:nth-child(3) > a > h2").text.strip
+        source.headline4_url = doc.search("body > div.tb-c-story-previews.tb-c-story-previews--homepage > div > div.tb-o-grid > div:nth-child(3) > a").attribute("href").value
+        source.headline5 = doc.search("body > div.tb-c-story-previews.tb-c-story-previews--homepage > div > div.tb-o-grid > div:nth-child(4) > a > h2").text.strip
+        source.headline5_url = doc.search("body > div.tb-c-story-previews.tb-c-story-previews--homepage > div > div.tb-o-grid > div:nth-child(4) > a").attribute("href").value
+
         source.headlines_ary = []
         source.headlines_ary << source.headline1
         source.headlines_ary << source.headline2
+        source.headlines_ary << source.headline3
+        source.headlines_ary << source.headline4
+        source.headlines_ary << source.headline5
 
         source.url_ary = []
         source.url_ary << source.headline1_url
         source.url_ary << source.headline2_url
+        source.url_ary << source.headline3_url
+        source.url_ary << source.headline4_url
+        source.url_ary << source.headline5_url
 
         Headlines::Source.new(source.name, source.url, source.headlines_ary, source.url_ary)
         source
